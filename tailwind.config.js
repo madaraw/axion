b71a1bdce3c03cbaa17f9615ab35641b993dcd56
madaraw <rachediwalid@yahoo.fr>
2023-04-30 14:01:41 +0100

    Configured font

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Roboto'],
+            },
+        },
+    },
+    plugins: [],
+}
