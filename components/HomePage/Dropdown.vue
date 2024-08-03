e88beea8f6b7e3712a74404655f4419abe817619
madaraw <rachediwalid@yahoo.fr>
2024-08-03 15:55:40 +0100

    styling

---
+++ components/HomePage/Dropdown.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="relative inline-block text-left text-gray-800">
+        <button @click="isMenuOpen = !isMenuOpen" type="button"
+            class="inline-flex items-center justify-between rounded-md">
+            <span class="font-bold material-symbols-outlined text-4xl">Menu</span>
+        </button>
+        <transition enter-active-class="transition ease-out duration-100" enter-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75"
+            leave-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+            <div v-if="isMenuOpen"
+                class="origin-top-right absolute right-0 top-8 mt-2 w-56 rounded-md shadow-lg text-sm overflow-hidden border z-20">
+                <div class="rounded-md bg-white shadow-xs">
+                    <div class="border-t border-gray-100"></div>
+                    <div class="py-1">
+                        <nuxt-link :to="{ path: '/', hash: '#pricing' }" @click.native="isMenuOpen = false"
+                            class="p-4 flex items-center space-x-2">
+                            <img src="https://s.svgbox.net/hero-outline.svg?ic=currency-rupee" class="h-6 w-6" />
+                            <span> Pricing </span>
+                        </nuxt-link>
+                        <nuxt-link :to="{ path: '/', hash: '#testimonials' }" @click.native="isMenuOpen = false"
+                            class="p-4 flex items-center space-x-2">
+                            <img src="https://s.svgbox.net/hero-outline.svg?ic=heart" class="h-6 w-6" />
+                            <span> Testimonials </span>
+                        </nuxt-link>
+                    </div>
+                    <div class="border-t border-gray-100"></div>
+                    <div class="py-1">
+                        <nuxt-link to="/login" @click.native="isMenuOpen = false"
+                            class="p-4 flex items-center space-x-2">
+                            <img src="https://s.svgbox.net/hero-outline.svg?ic=logout" class="h-6 w-6" />
+                            <span> Login </span>
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script setup>
+    const isMenuOpen = ref(false)
+</script>
---
+++ components/HomePage/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <hr class="sm:mx-20">
+    <footer class="sm:mx-20 py-12 flex flex-col sm:flex-row justify-between items-start">
+        <div>
+            <h2 class="text-2xl font-semibold mt-1 mb-20 ">Axion</h2>
+            <div class="flex">
+                <img src="../../assets/facebook logo.svg" alt="facebook logo" class="w-10 p-2">
+                <img src="../../assets/instagram logo.svg" alt="instagram logo" class="w-10 p-2">
+                <img src="../../assets/linkedin logo.svg" alt="linkedin logo" class="w-10 p-2">
+                <img src="../../assets/twitter logo.svg" alt="twitter logo" class="w-10 p-2">
+            </div>
+        </div>
+        <div class="flex space-x-8 w-1/2">
+            <div class="w-48 space-y-6">
+                <p>More about us</p>
+                <ul class="text-[#454545] space-y-6">
+                    <li><a href="#hero" class="hover:text-black">Home</a></li>
+                    <li><a href="#pricing" class="hover:text-black">Pricing</a></li>
+                    <li><a href="#testimonials" class="hover:text-black">Testimonials</a></li>
+
+                </ul>
+            </div>
+            <div class="w-48 space-y-6">
+                <p>People</p>
+                <ul class="text-[#454545] space-y-6">
+                    <li><a href="#" class="hover:text-black">Careers</a></li>
+                    <li><a href="#" class="hover:text-black">Contact us</a></li>
+                    <li><a href="#" class="hover:text-black">Work with us</a></li>
+                </ul>
+            </div>
+            <div class="w-48 space-y-6">
+                <p>More Links</p>
+                <ul class="text-[#454545] space-y-6">
+                    <li><a href="#" class="hover:text-black">Privacy</a></li>
+                    <li><a href="#" class="hover:text-black">Legal terms</a></li>
+                    <li><a href="#" class="hover:text-black">Something wrong?</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</template>
